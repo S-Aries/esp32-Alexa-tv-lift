78aaadcec00371991322d31348cdbefea818f412
Scot-Alan <hedricksa@gmail.com>
2019-11-18 11:46:09 -0600

    added actuator functionaliy

--- src/main.cpp
+++ src/main.cpp
@@ -24,7 +24,7 @@
 #include <IRutils.h>
 #include <IRrecv.h>
 #include <JC_Button.h>
-#include <ESP32Encoder.h>
+#include <Encoder.h>
 
 #define SERIAL_BAUDRATE 115200
 
@@ -41,12 +41,49 @@ fauxmoESP fauxmo;
 // Define stepper motor connections and motor interface type. Motor interface type must be set to 1 when using a driver
 Stepper stepper(STEPS, 13, 2); // Pin 13 connected to DIRECTION & Pin 2 connected to STEP Pin of Driver
 #define motorInterfaceType 1
+//const byte interruptPin[] = {21,23};
+
+const int RELAY[] = {14,27};  //RELAY[0] and RELAY[1] to access the pins; physicaly pins 31,30 respectively
+const int BTN_EXTEND = 0; //physicaly pin 6
+const int BTN_RETRACT = 4; //physical pin 7
+const uint8_t MANUAL = 1;    //a constant to indicate manual mode
+const uint8_t AUTOMATIC = 2; //a constant to indicate automatic mode
+const int BTN_MEM_PIN[] = {16,17,5}; //physicaly pins 8,9,10 respectively
+const int BTN_SET_MEM = 18; //physical pin 11
+
 const uint16_t kRecvPin = 15;
 
 IRrecv irrecv(kRecvPin);
 
 decode_results results;
 
+//Set up the linear actuator encoder
+//On many of the Arduino boards pins 2 and 3 are interrupt pins
+// which provide the best performance of the encoder data.
+Encoder myEnc(21, 23); //physicaly pins 14,18 respectively 
+long oldPosition  = -999;
+long targetPosition = 0;
+#define ACCURACY 10       //How close to your target position is close enough. Higher accuracy may result in 
+               // a bit of jitter as the actuator nears the position
+#define DEBOUNCE_MS 20     //A debounce time of 20 milliseconds usually works well for tactile button switches.
+#define PULLUP false        //To keep things simple, we use the Arduino's internal pullup resistor.
+#define INVERT true        //Since the pullup resistor will keep the pin high unless the
+               //switch is closed, this is negative logic, i.e. a high state
+               //means the button is NOT pressed. (Assuming a normally open switch.)
+uint8_t MODE = MANUAL;
+
+Button btnExtend(BTN_EXTEND, PULLUP, INVERT, DEBOUNCE_MS);
+Button btnRetract(BTN_RETRACT, PULLUP, INVERT, DEBOUNCE_MS);
+Button btnSetPos(BTN_SET_MEM, PULLUP, INVERT, DEBOUNCE_MS);
+Button btnPos1(BTN_MEM_PIN[0], PULLUP, INVERT, DEBOUNCE_MS);
+Button btnPos2(BTN_MEM_PIN[1], PULLUP, INVERT, DEBOUNCE_MS);
+Button btnPos3(BTN_MEM_PIN[2], PULLUP, INVERT, DEBOUNCE_MS);
+
+
+long memPosition[] = {0,0,0};
+void retractActuator();
+void extendActuator();
+void stopActuator();
 //////////flags/////////////
 bool shouldMove = false;
 bool shouldHome = false;
@@ -86,7 +123,7 @@ void homefunction() {
 }
 
 
-void flash(){//test for functonality, remove once completed
+/*void flash(){//test for functonality, remove once completed
 digitalWrite(led, HIGH);
 delay(1);
 digitalWrite(led, LOW);
@@ -94,7 +131,7 @@ delay(1000);
 digitalWrite(led, HIGH);
 delay(1000);
 digitalWrite(led, LOW);
-}
+}*/
 
 
 // Wi-Fi Connection
@@ -125,6 +162,15 @@ void setup() {
   // Wi-Fi connection
   wifiSetup();
 
+  pinMode(RELAY[0], OUTPUT);
+  pinMode(RELAY[1], OUTPUT);
+  btnExtend.begin();
+  btnRetract.begin();
+  btnSetPos.begin();
+  btnPos1.begin();
+  btnPos2.begin();
+  btnPos3.begin();
+  //Serial.begin(115200);
   irrecv.enableIRIn();
   pinMode(led, OUTPUT);
   pinMode(HALL_SENSOR_Tvhome_Front, INPUT);
@@ -159,7 +205,7 @@ void setup() {
   fauxmo.addDevice(LAMP_1);
   fauxmo.addDevice(LAMP_2);
   fauxmo.addDevice(TV_ROTATION);
-  fauxmo.addDevice(BLUE_LIGHT_SPECIAL);//test device remove on final version
+  //fauxmo.addDevice(BLUE_LIGHT_SPECIAL);//test device remove on final version
   
   fauxmo.onSetState([](unsigned char device_id, const char * device_name, bool state, unsigned char value){
  
@@ -197,13 +243,13 @@ void setup() {
         digitalWrite(SLEEP, LOW);
       }
     }
-    if ( (strcmp(device_name, BLUE_LIGHT_SPECIAL) == 0) ){
+    /*if ( (strcmp(device_name, BLUE_LIGHT_SPECIAL) == 0) ){
       if (state) {
         flash();
       }else{
         digitalWrite(led, LOW);
       }
-    }
+    }*/
     
   });
 }
@@ -214,7 +260,12 @@ void loop() {
   // Therefore, we have to manually poll for UDP packets
   fauxmo.handle();
   moveleft();
-  
+  btnExtend.read();
+  btnRetract.read();
+  btnSetPos.read();
+  btnPos1.read();
+  btnPos2.read();
+  btnPos3.read();
 //int Hallreading = digitalRead(HALL_SENSOR);  // for reading hall effect senor output, note that this negates home function
 //int hallreading = analogRead(HALL_SENSOR);
 //Serial.println(hallreading);
@@ -226,6 +277,75 @@ void loop() {
     Serial.printf("[MAIN] Free heap: %d bytes\n", ESP.getFreeHeap());
   }
     
+  if (btnExtend.isPressed()) {
+    extendActuator();
+    MODE = MANUAL;
+  }
+
+  if (btnRetract.isPressed()) {
+    retractActuator();
+    MODE = MANUAL;
+  }
+
+  if (!btnExtend.isPressed() && !btnRetract.isPressed() && MODE == MANUAL) {
+    stopActuator();
+    MODE = MANUAL;
+  }
+
+  if(btnPos1.wasReleased()) {
+    Serial.println("btnPos1");
+    MODE = AUTOMATIC;
+    targetPosition = memPosition[0]; 
+  }
+  if(btnPos2.wasReleased()) {
+    Serial.println("btnPos2");
+    MODE = AUTOMATIC;
+    targetPosition = memPosition[1]; 
+  }
+  if(btnPos3.wasReleased()) {
+    Serial.println("btnPos3");
+    MODE = AUTOMATIC;
+    targetPosition = memPosition[2]; 
+  }
+
+
+  //check the encoder to see if the position has changed
+  long newPosition = myEnc.read();
+  if (newPosition != oldPosition) {
+    oldPosition = newPosition;
+    Serial.println(newPosition);
+  }
+
+  if(MODE == AUTOMATIC && newPosition != targetPosition) {
+    Serial.print("Target/Actual:");Serial.print(targetPosition);Serial.print(" / ");Serial.print(newPosition);Serial.print(" [");Serial.print(abs(targetPosition - newPosition));Serial.println("]");
+    if(targetPosition > newPosition) {
+      Serial.println("AUTO RETRACT");
+      retractActuator();
+      MODE = AUTOMATIC;
+    }
+    if(targetPosition < newPosition) {
+      Serial.println("AUTO EXTEND");
+      extendActuator();
+      MODE = AUTOMATIC;
+    }
+    if( (targetPosition == newPosition) || abs(targetPosition - newPosition) <= ACCURACY) {
+      Serial.println("AUTO STOP");
+      stopActuator();
+      MODE = MANUAL;
+    }
+  }
+  
+  if(btnSetPos.isPressed()) {
+   if(btnPos1.isPressed())
+     memPosition[0] = newPosition;
+     //Serial.println("memposition1 set");
+   if(btnPos2.isPressed())
+     memPosition[1] = newPosition;
+   if(btnPos3.isPressed())
+     memPosition[2] = newPosition;
+       
+  }
+
   if(irrecv.decode(&results)) {
   switch(results.value)
   {
@@ -261,4 +381,22 @@ irrecv.resume();
   delay(1);
  }  
  
+}
+
+void extendActuator() {
+  //Serial.println("extendActuator");
+  digitalWrite(RELAY[0], HIGH);
+  digitalWrite(RELAY[1], LOW);
+}
+
+void retractActuator() {
+  //Serial.println("retractActuator");
+  digitalWrite(RELAY[0], LOW);
+  digitalWrite(RELAY[1], HIGH);
+}
+
+void stopActuator() {
+  //Serial.println("stopActuator");
+  digitalWrite(RELAY[0], HIGH);
+  digitalWrite(RELAY[1], HIGH);
 }
